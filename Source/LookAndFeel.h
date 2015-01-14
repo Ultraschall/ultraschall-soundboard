5b6cf25d872a3ece0f9f04fdcae7380754775298
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-01-14 15:54:54 +0100

    add some colors

--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -27,6 +27,90 @@ static const Colour ColourGridFadeOut = Colour::fromRGB(225, 123, 180);
 static const Colour ColourGridDefaultBackround = Colour::fromRGB(59, 59, 48);
 static const Colour ColourGridDefault = Colour::fromRGB(241, 231, 106);
 
+// Tomorrow Night
+static const Colour TomorrowNightBackground1 = Colour::fromRGB (29, 31, 33);
+static const Colour TomorrowNightBackground2 = Colour::fromRGB (40, 42, 46);
+static const Colour TomorrowNightBackground3 = Colour::fromRGB (55, 59, 65);
+static const Colour TomorrowNightForeground1 = Colour::fromRGB (197, 200, 198);
+static const Colour TomorrowNightForeground2 = Colour::fromRGB (150, 152, 150);
+static const Colour TomorrowNightRed = Colour::fromRGB (204, 102, 102);
+static const Colour TomorrowNightOrange = Colour::fromRGB (222, 147, 95);
+static const Colour TomorrowNightYellow = Colour::fromRGB (240, 198, 116);
+static const Colour TomorrowNightGreen = Colour::fromRGB (181, 189, 104);
+static const Colour TomorrowNightAqua = Colour::fromRGB (138, 190, 183);
+static const Colour TomorrowNightBlue = Colour::fromRGB (129, 162, 190);
+static const Colour TomorrowNightPurple = Colour::fromRGB (178, 148, 187);
+
+// Tomorrow
+static const Colour TomorrowBackground1 = Colour::fromRGB (255, 255, 255);
+static const Colour TomorrowBackground2 = Colour::fromRGB (239, 239, 239);
+static const Colour TomorrowBackground3 = Colour::fromRGB (214, 214, 214);
+static const Colour TomorrowForeground1 = Colour::fromRGB (77, 77, 76);
+static const Colour TomorrowForeground2 = Colour::fromRGB (142, 144, 140);
+static const Colour TomorrowRed = Colour::fromRGB (200, 40, 41);
+static const Colour TomorrowOrange = Colour::fromRGB (245, 135, 31);
+static const Colour TomorrowYellow = Colour::fromRGB (234, 183, 0);
+static const Colour TomorrowGreen = Colour::fromRGB (113, 140, 0);
+static const Colour TomorrowAqua = Colour::fromRGB (62, 153, 159);
+static const Colour TomorrowBlue = Colour::fromRGB (66, 113, 174);
+static const Colour TomorrowPurple = Colour::fromRGB (137, 89, 168);
+
+// Tomorrow Night Eighties
+static const Colour TomorrowNightEightiesBackground1 = Colour::fromRGB (45, 45, 45);
+static const Colour TomorrowNightEightiesBackground2 = Colour::fromRGB (57, 57, 57);
+static const Colour TomorrowNightEightiesBackground3 = Colour::fromRGB (81, 81, 81);
+static const Colour TomorrowNightEightiesForeground1 = Colour::fromRGB (204, 204, 204);
+static const Colour TomorrowNightEightiesForeground2 = Colour::fromRGB (153, 153, 153);
+static const Colour TomorrowNightEightiesRed = Colour::fromRGB (242, 119, 122);
+static const Colour TomorrowNightEightiesOrange = Colour::fromRGB (249, 145, 87);
+static const Colour TomorrowNightEightiesYellow = Colour::fromRGB (255, 204, 102);
+static const Colour TomorrowNightEightiesGreen = Colour::fromRGB (153, 204, 153);
+static const Colour TomorrowNightEightiesAqua = Colour::fromRGB (102, 204, 204);
+static const Colour TomorrowNightEightiesBlue = Colour::fromRGB (102, 153, 204);
+static const Colour TomorrowNightEightiesPurple = Colour::fromRGB (204, 153, 204);
+
+// Tomorrow Night Blue
+static const Colour TomorrowNightBlueBackground1 = Colour::fromRGB (0, 36, 81);
+static const Colour TomorrowNightBlueBackground2 = Colour::fromRGB (0, 52, 110);
+static const Colour TomorrowNightBlueBackground3 = Colour::fromRGB (0, 63, 142);
+static const Colour TomorrowNightBlueForeground1 = Colour::fromRGB (255, 255, 255);
+static const Colour TomorrowNightBlueForeground2 = Colour::fromRGB (114, 133, 183);
+static const Colour TomorrowNightBlueRed = Colour::fromRGB (255, 157, 164);
+static const Colour TomorrowNightBlueOrange = Colour::fromRGB (255, 197, 143);
+static const Colour TomorrowNightBlueYellow = Colour::fromRGB (255, 238, 173);
+static const Colour TomorrowNightBlueGreen = Colour::fromRGB (209, 241, 169);
+static const Colour TomorrowNightBlueAqua = Colour::fromRGB (153, 255, 255);
+static const Colour TomorrowNightBlueBlue = Colour::fromRGB (187, 218, 255);
+static const Colour TomorrowNightBluePurple = Colour::fromRGB (235, 187, 255);
+
+// Tomorrow Night Bright
+static const Colour TomorrowNightBrightBackground1 = Colour::fromRGB (0, 0, 0);
+static const Colour TomorrowNightBrightBackground2 = Colour::fromRGB (42, 42, 42);
+static const Colour TomorrowNightBrightBackground3 = Colour::fromRGB (66, 66, 66);
+static const Colour TomorrowNightBrightForeground1 = Colour::fromRGB (234, 234, 234);
+static const Colour TomorrowNightBrightForeground2 = Colour::fromRGB (150, 152, 150);
+static const Colour TomorrowNightBrightRed = Colour::fromRGB (213, 78, 83);
+static const Colour TomorrowNightBrightOrange = Colour::fromRGB (231, 140, 69);
+static const Colour TomorrowNightBrightYellow = Colour::fromRGB (231, 197, 71);
+static const Colour TomorrowNightBrightGreen = Colour::fromRGB (185, 202, 74);
+static const Colour TomorrowNightBrightAqua = Colour::fromRGB (112, 192, 177);
+static const Colour TomorrowNightBrightBlue = Colour::fromRGB (122, 166, 218);
+static const Colour TomorrowNightBrightPurple = Colour::fromRGB (195, 151, 216);
+
+
+static Colour ThemeBackground1 = TomorrowNightBackground1;
+static Colour ThemeBackground2 = TomorrowNightBackground2;
+static Colour ThemeBackground3 = TomorrowNightBackground3;
+static Colour ThemeForeground1 = TomorrowNightForeground1;
+static Colour ThemeForeground2 = TomorrowNightForeground2;
+static Colour ThemeRed = TomorrowNightRed;
+static Colour ThemeOrange = TomorrowNightOrange;
+static Colour ThemeYellow = TomorrowNightYellow;
+static Colour ThemeGreen = TomorrowNightGreen;
+static Colour ThemeAqua = TomorrowNightAqua;
+static Colour ThemeBlue = TomorrowNightBlue;
+static Colour ThemePurple = TomorrowNightPurple;
+
 class LookAndFeel_Ultraschall : public LookAndFeel_V3 {
 public:
     void drawTableHeaderBackground(Graphics& g,
