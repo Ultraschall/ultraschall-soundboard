84deca3d7086e849e38ac6462e858bb5f799c356
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-05-04 07:51:54 +0200

    update build script

--- build_mac.sh
+++ build_mac.sh
@@ -1,23 +1,23 @@
-git submodule init
-git submodule update
+git submodule init > /dev/null
+git submodule update > /dev/null
 
 cd Submodules/JUCE/
-git checkout master
-git pull
+git checkout master > /dev/null
+git pull > /dev/null
 cd ../../
 
 cd Submodules/danlin_modules
-git checkout master
-git pull
+git checkout master > /dev/null
+git pull > /dev/null
 cd ../../
 
-/Applications/Introjucer.app/Contents/MacOS/Introjucer --resave Standalone/Soundboard-Standalone.jucer
-/Applications/Introjucer.app/Contents/MacOS/Introjucer --resave Plugin/Soundboard-Plugin.jucer
+/Applications/Introjucer.app/Contents/MacOS/Introjucer --resave Standalone/Soundboard-Standalone.jucer > /dev/null
+/Applications/Introjucer.app/Contents/MacOS/Introjucer --resave Plugin/Soundboard-Plugin.jucer > /dev/null
 
 cd Standalone/Builds/MacOSX/
-xcodebuild
+xcodebuild > /dev/null
 cd ../../../
 
 cd Plugin/Builds/MacOSX/
-xcodebuild
+xcodebuild > /dev/null
 cd ../../../
