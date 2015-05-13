65bba8c5f836fe0ced560ba26f0639a23d547d0e
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-05-13 19:36:14 +0200

    cleanup project structure

---
+++ Source/Shared/UserInterface/Settings/SettingsWindow.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    SettingsWindow.h
+    Created: 13 May 2015 7:33:11pm
+    Author:  Daniel Lindenfelser
+
+  ==============================================================================
+*/
+
+#ifndef SETTINGSWINDOW_H_INCLUDED
+#define SETTINGSWINDOW_H_INCLUDED
+
+
+
+
+
+#endif  // SETTINGSWINDOW_H_INCLUDED
