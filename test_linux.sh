78d5510f56414f7844b3fdf3b5eb1aa476b6ec83
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-05-18 06:47:35 +0200

    add linux test script

---
+++ test_linux.sh
@@ -0,0 +1,30 @@
+echo "Update Git Submodules"
+git submodule init > /dev/null
+git submodule update > /dev/null
+
+cd Submodules/JUCE/
+git checkout master > /dev/null
+git pull > /dev/null
+cd ../../
+
+cd Submodules/danlin_modules
+git checkout master > /dev/null
+git pull > /dev/null
+cd ../../
+
+echo "Bootstrap Tools"
+cd Submodules/JUCE/extras/Introjucer/Builds/Linux/
+make -j4
+cd ../../../../../../
+
+echo "Update Projects"
+./Submodules/JUCE/extras/Introjucer/Builds/Linux/build/Introjucer --resave Projects/Tests/Tests.jucer > /dev/null
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+echo "UnitTesting"
+cd Projects/Tests/Builds/Linux/
+make -j4
+cd ../../../../
+./Projects/Tests/Builds/Linux/build/Tests
