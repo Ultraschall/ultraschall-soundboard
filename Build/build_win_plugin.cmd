78eb2fdfbc997088db181c97b178e0ebc0ad6399
Heiko Panjas <heiko@panjas.com>
2017-02-19 00:09:11 +0100

    add plugin-only build script

---
+++ Build/build_win_plugin.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+echo "Update Git Submodules"
+git submodule init > NUL
+git submodule update > NUL
+
+echo "Bootstrap Tools"
+msbuild /clp:ErrorsOnly;ShowTimestamp /t:rebuild /p:Configuration=Release /p:Platform="x64" /m /nologo "./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/Projucer.sln"
+
+echo "Update Projects"
+"./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/x64/Release/Projucer.exe" --resave Projects/Plugin/Plugin.jucer
+
+echo "Build Plugin 64bit"
+msbuild /clp:ErrorsOnly;ShowTimestamp /t:rebuild /p:Configuration=Release /p:Platform="x64" /m /nologo "./Projects/Plugin/Builds/VisualStudio2015/Plugin.sln"
+
+rem echo "Build Installer"
+rem msbuild /clp:ErrorsOnly;ShowTimestamp /property:Configuration=Release /m /nologo "./Projects/Installer/Installer.wixproj"
