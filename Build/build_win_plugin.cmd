b7cfbaae2bff7e0ae01620d2b069d1b04f437053
Heiko Panjas <heiko@panjas.com>
2017-02-19 10:25:00 +0100

    add plugin-only build scripts

--- Build/build_win_plugin.cmd
+++ Build/build_win_plugin.cmd
@@ -5,13 +5,11 @@ git submodule init > NUL
 git submodule update > NUL
 
 echo "Bootstrap Tools"
-msbuild /clp:ErrorsOnly;ShowTimestamp /t:rebuild /p:Configuration=Release /p:Platform="x64" /m /nologo "./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/Projucer.sln"
+msbuild /clp:ErrorsOnly;ShowTimestamp /t:build /p:Configuration=Release /p:Platform="x64" /m /nologo "./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/Projucer.sln"
 
 echo "Update Projects"
 "./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/x64/Release/Projucer.exe" --resave Projects/Plugin/Plugin.jucer
 
 echo "Build Plugin 64bit"
-msbuild /clp:ErrorsOnly;ShowTimestamp /t:rebuild /p:Configuration=Release /p:Platform="x64" /m /nologo "./Projects/Plugin/Builds/VisualStudio2015/Plugin.sln"
+msbuild /clp:ErrorsOnly;ShowTimestamp /t:build /p:Configuration=Release /p:Platform="x64" /m /nologo "./Projects/Plugin/Builds/VisualStudio2015/Plugin.sln"
 
-rem echo "Build Installer"
-rem msbuild /clp:ErrorsOnly;ShowTimestamp /property:Configuration=Release /m /nologo "./Projects/Installer/Installer.wixproj"
