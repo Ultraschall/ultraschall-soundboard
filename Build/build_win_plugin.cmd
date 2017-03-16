6ac2ae9b1281482de385fb67eb3758213432b1f9
Heiko Panjas <heiko@panjas.com>
2017-03-16 20:52:56 +0100

    upgrade windows build to visual studio 2017

--- Build/build_win_plugin.cmd
+++ Build/build_win_plugin.cmd
@@ -10,6 +10,9 @@ msbuild /clp:ErrorsOnly;ShowTimestamp /t:build /p:Configuration=Release /p:Platf
 echo "Update Projects"
 "./Submodules/JUCE/extras/Projucer/Builds/VisualStudio2015/x64/Release/Projucer.exe" --resave Projects/Plugin/Plugin.jucer
 
+echo "Upgrade Projects to Visual Studio 2017"
+devenv .\Projects\Plugin\Builds\VisualStudio2015\Plugin.sln /Upgrade
+
 echo "Build Plugin 64bit"
 msbuild /clp:ErrorsOnly;ShowTimestamp /t:build /p:Configuration=Release /p:Platform="x64" /m /nologo "./Projects/Plugin/Builds/VisualStudio2015/Plugin.sln"
 
